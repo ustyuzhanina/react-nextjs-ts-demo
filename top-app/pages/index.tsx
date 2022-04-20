126ad0282f83c82d035e6b9e631916cab12ef7c0
ustyuzhanina <elena.ustyuzhanina.rus@gmail.com>
2022-04-20 20:34:36 +0700

    add classnames library

--- top-app/pages/index.tsx
+++ top-app/pages/index.tsx
@@ -1,9 +1,10 @@
+import { Htag } from "../components";
 
 
 export default function Home(): JSX.Element {
   return (
     <div>
-      test of styles
+      <Htag tag={'h1'}>Курсы по Photoshop</Htag>
     </div>
   );
 }
