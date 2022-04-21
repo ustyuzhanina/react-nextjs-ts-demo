a6620fb97f8da6d5d884a0fe2de826250dcfd791
ustyuzhanina <elena.ustyuzhanina.rus@gmail.com>
2022-04-21 21:33:40 +0700

    feat: start lesson HTML props

--- top-app/components/Button/Button.tsx
+++ top-app/components/Button/Button.tsx
@@ -5,8 +5,8 @@ import cn from 'classnames';
 export const Button = ({ appearance, children }: ButtonProps): JSX.Element => {
   return (
     <button className={cn(styles.button, {
-      [styles.primary]: appearance = 'primary',
-      [styles.ghost]: appearance = 'ghost',
+      [styles.primary]: appearance == 'primary',
+      [styles.ghost]: appearance == 'ghost',
     })}
     >
       {children}
--- top-app/pages/index.tsx
+++ top-app/pages/index.tsx
@@ -6,7 +6,8 @@ export default function Home(): JSX.Element {
   return (
     <div>
       <Htag tag={'h1'}>Курсы по Photoshop</Htag>
-      
+      <Button appearance='primary'>Кнопка</Button>
+      <Button appearance='ghost'>Кнопка</Button>
     </div>
   );
 }
