edcf68664a3a46bb8a3ede1d1c739711f0c9b925
ustyuzhanina <elena.ustyuzhanina.rus@gmail.com>
2022-04-21 15:24:06 +0700

    feat: create sass variables and change next.js config to make them available globally

--- top-app/next.config.js
+++ top-app/next.config.js
@@ -1,6 +1,11 @@
+const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+        prependData: `@import 'variables.scss';`
+    }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- top-app/pages/index.tsx
+++ top-app/pages/index.tsx
@@ -1,10 +1,12 @@
 import { Htag } from "../components";
+import { Button } from "../components";
 
 
 export default function Home(): JSX.Element {
   return (
     <div>
       <Htag tag={'h1'}>Курсы по Photoshop</Htag>
+      
     </div>
   );
 }
