7e305d1c9dd256ea3f8fe9c8a78921ace7a5f7a9
ustyuzhanina <elena.ustyuzhanina.rus@gmail.com>
2022-04-22 11:42:10 +0700

    feat: add @svgr/webpack dependancy and required configs

--- top-app/next.config.js
+++ top-app/next.config.js
@@ -5,7 +5,35 @@ const nextConfig = {
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
         prependData: `@import 'variables.scss';`
-    }
+    },
+    webpack(config, options) {
+        config.module.rules.push({
+            loader: '@svgr/webpack',
+            issuer: /\.[jt]sx?$/,
+            options: {
+                prettier: false,
+                svgo: true,
+                svgoConfig: {
+                    //     plugins: [{
+                    //         removeViewBox: false
+                    //     }],
+                    // },
+                    plugins: [{
+                        name: 'preset-default',
+                        params: {
+                            override: {
+                                removeViewBox: false
+                            }
+                        }
+                    }],
+                },
+                titleProp: true,
+            },
+            test: /\.svg$/,
+        });
+
+        return config;
+    },
 };
 
 module.exports = nextConfig;
--- top-app/pages/index.tsx
+++ top-app/pages/index.tsx
@@ -6,8 +6,8 @@ export default function Home(): JSX.Element {
   return (
     <div>
       <Htag tag={'h1'}>Курсы по Photoshop</Htag>
-      <Button appearance='primary'>Кнопка</Button>
-      <Button appearance='ghost'>Кнопка</Button>
+      <Button appearance='primary' arrow="right">Кнопка</Button>
+      <Button appearance='ghost' arrow="down">Кнопка</Button>
     </div>
   );
 }
